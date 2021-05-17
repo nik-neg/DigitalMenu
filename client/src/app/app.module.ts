d3ee7a4a08a41531a1456b85c611801fd6beda56
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-17 11:59:40 +0200

    build: setup dashboard, restaurant, menu, dish component and api client service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishComponent } from './dish/dish.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    RestaurantComponent,
+    MenuComponent,
+    DishComponent
   ],
   imports: [
     BrowserModule,
