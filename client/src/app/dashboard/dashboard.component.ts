ff5390b9ed0335b8e137865740e419550ca26b1d
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-21 13:13:34 +0200

    refactor: rename images and paths to local images

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -11,14 +11,14 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 export class DashboardComponent implements OnInit {
   title = 'ng-carousel-demo';
   images = [
-    {title: 'Cuba Life', short: 'Madrid - Visit us', src: "../../assets/images/restaurants/1.jpg"},
-    {title: 'Burger Bar', short: 'Miami - Come and try our Big Deal :)', src: "./../assets/images/restaurants/2.jpg"},
-    {title: 'Delicat', short: 'Tel Aviv Marina - Always good', src: "./../assets/images/restaurants/3.jpg"},
-    {title: 'The Spot', short: 'Nizza - Try our delicous kitchen', src: "../../assets/images/restaurants/4.jpg"},
-    {title: 'Mar e Sol', short: 'Lissabon - Come to see the sea', src: "./../assets/images/restaurants/5.jpg"},
-    {title: 'Green Lounge', short: 'Tokyo - Open from 10 a.m. to 11 p.m', src: "./../assets/images/restaurants/6.jpg"},
-    {title: 'La Rustica', short: 'Rome - Cucina italiana tradizionale', src: "../../assets/images/restaurants/7.jpg"},
-    {title: 'Surf & Turf Bar', short: 'San Francisco - Come and try our shrimps', src: "./../assets/images/restaurants/8.jpg"},
+    {title: 'Cuba Life', short: 'Madrid - Visit us', src: "../../assets/images/restaurants/la_cuba.jpg"},
+    {title: 'Burger Bar', short: 'Miami - Come and try our Big Deal :)', src: "./../assets/images/restaurants/burger_bar.jpg"},
+    {title: 'Delicat', short: 'Tel Aviv Marina - Always good', src: "./../assets/images/restaurants/delicat.jpg"},
+    {title: 'The Spot', short: 'Nizza - Try our delicous kitchen', src: "../../assets/images/restaurants/the_spot.jpg"},
+    {title: 'Mar e Sol', short: 'Lissabon - Come to see the sea', src: "./../assets/images/restaurants/mar_e_sol.jpg"},
+    {title: 'Green Lounge', short: 'Tokyo - Open from 10 a.m. to 11 p.m', src: "./../assets/images/restaurants/green_lounge.jpg"},
+    {title: 'La Rustica', short: 'Rome - Cucina italiana tradizionale', src: "../../assets/images/restaurants/la_rustica.jpg"},
+    {title: 'Surf & Turf Bar', short: 'San Francisco - Come and try our shrimps', src: "./../assets/images/restaurants/surf_and_turf_bar.jpg"},
   ];
 
   private restaurantsUrl = "restaurants";
@@ -26,7 +26,7 @@ export class DashboardComponent implements OnInit {
 
   constructor(private apiClient: ApiClientService, config: NgbCarouselConfig) {
     this.restaurants = [];
-    config.interval = 3000;
+    config.interval = 4000;
     config.keyboard = true;
     config.pauseOnHover = true;
   }
