eebb3eb696c5834ea26224b3bd815e65d70e79d0
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-21 22:36:13 +0200

    fix: make card title of one card shorter

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -15,7 +15,7 @@ export class DashboardComponent implements OnInit {
     {title: 'Burger Bar', slogan: 'Miami - Try our Big Deal :)', imagePath: "./../assets/images/restaurants/burger_bar.jpg"},
     {title: 'Delicat', slogan: 'Tel Aviv Marina - Enjoy!', imagePath: "./../assets/images/restaurants/delicat.jpg"},
     {title: 'The Spot', slogan: 'Nizza - Magnifique cuisine', imagePath: "../../assets/images/restaurants/the_spot.jpg"},
-    {title: 'Mar e Sol', slogan: 'Lissabon - Come to see the sea', imagePath: "./../assets/images/restaurants/mar_e_sol.jpg"},
+    {title: 'Mar e Sol', slogan: 'Lissabon - Have a seat', imagePath: "./../assets/images/restaurants/mar_e_sol.jpg"},
     {title: 'Green Lounge', slogan: 'Tokyo - From 10 a.m. to 11 p.m', imagePath: "./../assets/images/restaurants/green_lounge.jpg"},
     {title: 'La Rustica', slogan: 'Rome - Cucina italiana', imagePath: "../../assets/images/restaurants/la_rustica.jpg"},
     {title: 'Surf & Turf Bar', slogan: 'San Francisco - Best shrimps', imagePath: "./../assets/images/restaurants/surf_and_turf_bar.jpg"},
