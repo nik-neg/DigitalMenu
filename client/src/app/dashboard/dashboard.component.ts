30dfbfbca6c7614dd769a57960a0dcfc3f309cb6
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-21 20:04:38 +0200

    style: add cards, change images

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -12,13 +12,13 @@ export class DashboardComponent implements OnInit {
   title = 'ng-carousel-demo';
   images = [
     {title: 'Cuba Life', slogan: 'Madrid - Visit us', imagePath: "../../assets/images/restaurants/la_cuba.jpg"},
-    {title: 'Burger Bar', slogan: 'Miami - Come and try our Big Deal :)', imagePath: "./../assets/images/restaurants/burger_bar.jpg"},
-    {title: 'Delicat', slogan: 'Tel Aviv Marina - Always good', imagePath: "./../assets/images/restaurants/delicat.jpg"},
-    {title: 'The Spot', slogan: 'Nizza - Try our delicous kitchen', imagePath: "../../assets/images/restaurants/the_spot.jpg"},
+    {title: 'Burger Bar', slogan: 'Miami - Try our Big Deal :)', imagePath: "./../assets/images/restaurants/burger_bar.jpg"},
+    {title: 'Delicat', slogan: 'Tel Aviv Marina - Enjoy!', imagePath: "./../assets/images/restaurants/delicat.jpg"},
+    {title: 'The Spot', slogan: 'Nizza - Magnifique cuisine', imagePath: "../../assets/images/restaurants/the_spot.jpg"},
     {title: 'Mar e Sol', slogan: 'Lissabon - Come to see the sea', imagePath: "./../assets/images/restaurants/mar_e_sol.jpg"},
-    {title: 'Green Lounge', slogan: 'Tokyo - Open from 10 a.m. to 11 p.m', imagePath: "./../assets/images/restaurants/green_lounge.jpg"},
-    {title: 'La Rustica', slogan: 'Rome - Cucina italiana tradizionale', imagePath: "../../assets/images/restaurants/la_rustica.jpg"},
-    {title: 'Surf & Turf Bar', slogan: 'San Francisco - Come and try our shrimps', imagePath: "./../assets/images/restaurants/surf_and_turf_bar.jpg"},
+    {title: 'Green Lounge', slogan: 'Tokyo - From 10 a.m. to 11 p.m', imagePath: "./../assets/images/restaurants/green_lounge.jpg"},
+    {title: 'La Rustica', slogan: 'Rome - Cucina italiana', imagePath: "../../assets/images/restaurants/la_rustica.jpg"},
+    {title: 'Surf & Turf Bar', slogan: 'San Francisco - Best shrimps', imagePath: "./../assets/images/restaurants/surf_and_turf_bar.jpg"},
   ];
 
   private restaurantsUrl = "restaurants";
