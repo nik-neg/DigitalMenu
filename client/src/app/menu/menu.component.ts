c1b847d5e598e9f7d90c93263efe71d9adb98f50
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-23 12:53:28 +0200

    refactor: menu card in restaurant details

--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Menu } from '../menu/entities/menu';
-import { ActivatedRoute, Params, ParamMap  } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -9,26 +10,42 @@ import { ActivatedRoute, Params, ParamMap  } from '@angular/router';
 export class MenuComponent implements OnInit {
   @Input() menu: Menu;
   isAdmin: boolean = false
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private alertController: AlertController) {
     this.menu = new Menu();
    }
 
   ngOnInit(): void {
-    this.getRestaurantDetails();
+    this.getMenuDetails();
   }
-  getRestaurantDetails(): void {
+  getMenuDetails(): void {
     this.route.queryParams.subscribe(params => {
       this.isAdmin = params['isAdmin'] === 'true' ? true : false;
     });
+    // this.alertWrapper();
   }
+  // async presentAlert() {
+  //   const alert = await this.alertController.create({
+  //     cssClass: 'basic-alert',
+  //     header: 'Alert Header',
+  //     subHeader: 'Alert Subtitle',
+  //     message: 'This is an alert message.',
+  //     buttons: ['OK']
+  //   });
+
+  //   await alert.present();
+  // }
 
   editURL() {
     if (this.isAdmin) {
       return `/restaurants/${this.menu.restaurant}/menu/${this.menu._id}`;
     } else {
-      // alert('only admins can edit menus');
-      return '/'; // pass param to route ?
+      // this.alertWrapper();
+      return '/';
     }
   }
+
+  // async alertWrapper() {
+  //   await this.presentAlert();
+  // }
 
 }
