7842fc52aba1499d7ef04f9a86b77c039421a469
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-23 10:43:07 +0200

    style: change color theme and card design, add color variables to styles

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'client';
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#96bb7c';
+ }
 }
