0c60b915a77d2cd7d4625f7398de3d7a1b4cb876
Alexander Genkin <alexander_genkin@gmx.net>
2021-07-21 23:37:10 +0200

    refactor: login page with register and login functionality, adjusted css"

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login: boolean = false;
+  loginMessage : string = 'Have already an account? Sign in!';
+  registerMessage : string = 'Not yet registered? Welcome and enjoy!'
+
+  constructor() {
+  }
+
+  signIn(event: any) {
+    event.preventDefault();
+    this.login = !this.login ;
+  }
 
   ngOnInit(): void {
+
   }
 
 }
